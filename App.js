44a6f32a07d96eb14590200296d8c719e4726a21
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-01-29 13:21:24 +0530

    same two files inside component

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+export default function App() {
+    const [isImportant, setIsImportant] = React.useState("Yes")
+    /**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+    
+    function handleClick() {
+      const isImportant1 = "No"
+       setIsImportant(prevState=>[isImportant1])   // it's working fine but another method very simple and just easy way. see belowblow 
+    }
+   
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value" onClick={handleClick}>
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
+}
+import React from "react"
+
+export default function App() {
+    const [isImportant, setIsImportant] = React.useState("Yes")
+    /**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+    
+    function handleClick() {
+        setIsImportant("No")  //Very simple 
+    }
+    
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value" onClick={handleClick}>
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
+}
